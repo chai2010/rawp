588b104a0e66e38e013160719170ee9e9adeb9c2
chai2010 <chaishushan@gmail.com>
2015-06-19 07:37:14 +0800

    more builtin func

--- builtin.go
+++ builtin.go
@@ -6,7 +6,59 @@ package rawp
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"strings"
 )
+
+func callerFileLine() (file string, line int) {
+	_, file, line, ok := runtime.Caller(2)
+	if ok {
+		// Truncate file name at last file name separator.
+		if index := strings.LastIndex(file, "/"); index >= 0 {
+			file = file[index+1:]
+		} else if index = strings.LastIndex(file, "\\"); index >= 0 {
+			file = file[index+1:]
+		}
+	} else {
+		file = "???"
+		line = 1
+	}
+	return
+}
+
+func assert(condition bool, args ...interface{}) {
+	if !condition {
+		file, line := callerFileLine()
+		if msg := fmt.Sprint(args...); msg != "" {
+			fmt.Fprintf(os.Stderr, "%s:%d: Assert failed, %s", file, line, msg)
+		} else {
+			fmt.Fprintf(os.Stderr, "%s:%d: Assert failed", file, line)
+		}
+		os.Exit(1)
+	}
+}
+
+func panic(v ...interface{}) {
+	log.Panic(v...)
+}
+
+func panicf(format string, v ...interface{}) {
+	log.Panicf(format, a...)
+}
+
+func panicln(v ...interface{}) {
+	log.Panicln(v...)
+}
+
+func logf(format string, v ...interface{}) {
+	log.Printf(format, v...)
+}
+
+func logln(v ...interface{}) {
+	log.Println(v...)
+}
 
 func errorf(format string, a ...interface{}) error {
 	return fmt.Errorf(format, a...)
